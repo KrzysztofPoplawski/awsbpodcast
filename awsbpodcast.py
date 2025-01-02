cf7c3ac5651d0f0dc64338fe99e5c7530f54c6c2
Krzysztof Popławski <kpoplawski@wsb.edu.pl>
2025-01-02 15:17:55 +0100

    update

--- awsbpodcast.py
+++ awsbpodcast.py
@@ -3,47 +3,47 @@ import xml.etree.ElementTree as ET
 from flask import Flask, Response
 
 app = Flask(__name__)
+def gen_pod():
+    # URL do istniejącego feedu RSS YouTube
+    YOUTUBE_RSS_URL = "https://www.youtube.com/feeds/videos.xml?playlist_id=PLf8XERBV_Iuv4-YfCd7ucWbe44usUikJ6"
 
-# URL do istniejącego feedu RSS YouTube
-YOUTUBE_RSS_URL = "https://www.youtube.com/feeds/videos.xml?playlist_id=PLf8XERBV_Iuv4-YfCd7ucWbe44usUikJ6"
+    # Zdefiniuj przestrzenie nazw
+    namespaces = {
+        'yt': 'http://www.youtube.com/xml/schemas/2015',
+        'media': 'http://search.yahoo.com/mrss/',
+        'atom': 'http://www.w3.org/2005/Atom'
+    }
 
-# Zdefiniuj przestrzenie nazw
-namespaces = {
-    'yt': 'http://www.youtube.com/xml/schemas/2015',
-    'media': 'http://search.yahoo.com/mrss/',
-    'atom': 'http://www.w3.org/2005/Atom'
-}
+    # Pobierz dane z YouTube RSS
+    response = requests.get(YOUTUBE_RSS_URL)
+    response.raise_for_status()
+    youtube_rss = response.content
 
-# Pobierz dane z YouTube RSS
-response = requests.get(YOUTUBE_RSS_URL)
-response.raise_for_status()
-youtube_rss = response.content
+    # Parsuj oryginalny RSS
+    root = ET.fromstring(youtube_rss)
 
-# Parsuj oryginalny RSS
-root = ET.fromstring(youtube_rss)
+    # Utwórz nowy RSS 2.0
+    rss = ET.Element("rss", version="2.0")
+    channel = ET.SubElement(rss, "channel")
 
-# Utwórz nowy RSS 2.0
-rss = ET.Element("rss", version="2.0")
-channel = ET.SubElement(rss, "channel")
+    # Dodaj informacje o kanale
+    ET.SubElement(channel, "title").text = root.find("./atom:title", namespaces).text
+    ET.SubElement(channel, "link").text = root.find("./atom:link", namespaces).attrib["href"]
+    ET.SubElement(channel, "description").text = "Poprawiony RSS z YouTube playlist"
 
-# Dodaj informacje o kanale
-ET.SubElement(channel, "title").text = root.find("./atom:title", namespaces).text
-ET.SubElement(channel, "link").text = root.find("./atom:link", namespaces).attrib["href"]
-ET.SubElement(channel, "description").text = "Poprawiony RSS z YouTube playlist"
+    # Dodaj każdy wpis z XML jako element RSS
+    for entry in root.findall("./atom:entry", namespaces):
+        item = ET.SubElement(channel, "item")
+        ET.SubElement(item, "title").text = entry.find("./atom:title", namespaces).text
+        ET.SubElement(item, "link").text = entry.find("./atom:link", namespaces).attrib["href"]
+        ET.SubElement(item, "description").text = entry.find("./media:group/media:description", namespaces).text
+        ET.SubElement(item, "pubDate").text = entry.find("./atom:published", namespaces).text
 
-# Dodaj każdy wpis z XML jako element RSS
-for entry in root.findall("./atom:entry", namespaces):
-    item = ET.SubElement(channel, "item")
-    ET.SubElement(item, "title").text = entry.find("./atom:title", namespaces).text
-    ET.SubElement(item, "link").text = entry.find("./atom:link", namespaces).attrib["href"]
-    ET.SubElement(item, "description").text = entry.find("./media:group/media:description", namespaces).text
-    ET.SubElement(item, "pubDate").text = entry.find("./atom:published", namespaces).text
+    # Zapisz do pliku
+    tree = ET.ElementTree(rss)
+    tree.write("corrected_youtube_rss.xml", encoding="utf-8", xml_declaration=True)
 
-# Zapisz do pliku
-tree = ET.ElementTree(rss)
-tree.write("corrected_youtube_rss.xml", encoding="utf-8", xml_declaration=True)
-
-print("Poprawiony RSS został zapisany w pliku 'corrected_youtube_rss.xml'")
+#    print("Poprawiony RSS został zapisany w pliku 'corrected_youtube_rss.xml'")
 
 
 @app.route("/favicon.ico")
@@ -51,13 +51,14 @@ def favicon():
     return "", 204  # Pusta odpowiedź, brak zawartości
 
 # Domyślna ścieżka, np. strona informacyjna
-@app.route("/")
+@app.route("/rss")
 def home():
     return "To jest serwer RSS. Użyj ścieżki /rss, aby uzyskać kanał RSS."
 
 # Endpoint dla RSS
-@app.route("/rss")
+@app.route("/")
 def serve_rss():
+    gen_pod()
     try:
         with open("corrected_youtube_rss.xml", "r", encoding="utf-8") as file:
             rss_content = file.read()
